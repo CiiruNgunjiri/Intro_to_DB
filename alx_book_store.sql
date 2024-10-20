30e8ea053704e2776760fb73554ad39315229411
Linda <ciiru.ngunjiri@gmail.com>
2024-10-20 12:52:11 +0300

    modified database schema for alx_book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,13 +1,13 @@
 """create a book_store database"""
 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 """create tables named books, authors, customers, orders, order_details respectively"""
 
 """stores information about books avaliable in the bookstore"""
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY , 
     title VARCHAR(130) NOT NULL, 
     author_id INT FOREIGN KEY REFERENCES Authors(Author_id), 
@@ -17,14 +17,14 @@ CREATE TABLE Books (
 
 """stores information about authors"""
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR (215)
 );
 
 """stores information about customers"""
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -33,7 +33,7 @@ CREATE TABLE Customers (
 
 """stores information about orders placed by customers"""
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id FOREIGN KEY REFERENCES Customers(customer_id),
     order_date DATE
@@ -41,7 +41,7 @@ CREATE TABLE Orders (
 
 """stores information about the books included in each order"""
 
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id FOREIGN KEY REFERENCES Orders(order_id),
     book_id FOREIGN KEY REFERENCES Books(book_id),

51fda035414a8ab98f4f2537bf644ec84e3d8974
Linda <ciiru.ngunjiri@gmail.com>
2024-10-20 21:37:26 +0300

    added task_5.sql file

---
+++ task_5.sql
@@ -0,0 +1,10 @@
+--use the database provided
+USE alx_book_store;
+
+SELECT TABLE Customer (customer_id, customer_name, email, address) 
+VALUE (
+    customer_id = 1
+    customer_name = "Cole Baidon"
+    email = "cbaidoo@sandtech.com"
+    address = "123 Happiness Ave." 
+); 

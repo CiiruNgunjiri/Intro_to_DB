730770079ed4739b3f70fbfe959a0b07e790d28f
Linda <ciiru.ngunjiri@gmail.com>
2024-10-20 21:46:16 +0300

    modified task_5.sql file

--- task_5.sql
+++ task_5.sql
@@ -4,4 +4,4 @@ USE alx_book_store;
 --insert dat values for s single row
 
 INSERT INTO customer (customer_id, customer_name, email, address) 
-VALUES (1, "Cole Baidon", "cbaidoo@sandtech.com", "123 Happiness Ave."); 
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."); 

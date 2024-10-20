a96af21a807b0930f612578309c2ae93b8de0b03
Linda <ciiru.ngunjiri@gmail.com>
2024-10-20 21:27:55 +0300

    added task_4.sql file

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Use the specified database
+USE alx_book_store;
+
+-- Query to retrieve full description of the books table
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
